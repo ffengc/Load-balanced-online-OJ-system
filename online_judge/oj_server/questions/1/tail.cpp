ce84085877f9ad4f19acec2c2f0a525dd9bd0ae5
Yufccode <yufc779@gmail.com>
2024-08-26 14:49:10 +0800

    fix docs

--- online_judge/oj_server/questions/1/tail.cpp
+++ online_judge/oj_server/questions/1/tail.cpp
@@ -5,41 +5,38 @@
 
 void Test1()
 {
-    // 通过定义临时对象，来完成方法的调用
     int ret = Solution().MyStrlen("hello world");
     if (ret == 11)
     {
-        std::cout << "[exp1][input:\"hello world\"] 通过" << std::endl;
+        std::cout << "[exp1][input:\"hello world\"] pass" << std::endl;
     }
     else
     {
-        std::cout << "[exp1][input:\"hello world\"] 答案错误" << std::endl;
+        std::cout << "[exp1][input:\"hello world\"] wrong" << std::endl;
     }
 }
 void Test2()
 {
-    // 通过定义临时对象，来完成方法的调用
     int ret = Solution().MyStrlen("aaaabbbbcccc");
     if (ret == 12)
     {
-        std::cout << "[exp2][input:\"aaaabbbbcccc\"] 通过" << std::endl;
+        std::cout << "[exp2][input:\"aaaabbbbcccc\"] pass" << std::endl;
     }
     else
     {
-        std::cout << "[exp2][input:\"aaaabbbbcccc\"] 答案错误" << std::endl;
+        std::cout << "[exp2][input:\"aaaabbbbcccc\"] wrong" << std::endl;
     }
 }
 void Test3()
 {
-    // 通过定义临时对象，来完成方法的调用
     int ret = Solution().MyStrlen("");
     if (ret == 0)
     {
-        std::cout << "[exp3][input:\"\"] 通过" << std::endl;
+        std::cout << "[exp3][input:\"\"] pass" << std::endl;
     }
     else
     {
-        std::cout << "[exp3][input:\"\"] 答案错误" << std::endl;
+        std::cout << "[exp3][input:\"\"] wrong" << std::endl;
     }
 }
 int main()
